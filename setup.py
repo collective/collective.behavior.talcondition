9f083f21fd2e66ac237eb7fadc6d03907a4e44a2
Gauthier Bastien <g.bastien@imio.be>
2015-09-16 16:41:19 +0200

    Back to development: 0.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.behavior.talcondition',
-    version='0.4',
+    version='0.5.dev0',
     description="This package contains a Dexterity behavior and AT schemaextender to add a TAL condition on a content type.",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
