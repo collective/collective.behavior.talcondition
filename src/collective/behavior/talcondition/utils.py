1799297aab2b862111fb1240f38e5927049236e9
Christophe Boulanger <christophe.boulanger@imio.be>
2021-06-16 10:47:41 +0200

    Fix deprecated import

--- src/collective/behavior/talcondition/utils.py
+++ src/collective/behavior/talcondition/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from App.class_init import InitializeClass
+from AccessControl.class_init import InitializeClass
 from collective.behavior.talcondition import PLONE_VERSION
 from plone import api
 from Products.CMFCore.Expression import createExprContext
